28e3254aceb21d574f4bb8033b301edcbc87a70c
Cyber Neko <firexneko7@gmail.com>
2023-05-15 00:22:22 +0900

    Issue#3の修正

--- output/question1.php
+++ output/question1.php
@@ -1,13 +1,20 @@
 <?php
-    require 'Car.php';
-    require 'Honda.php';
-    require 'Nissan.php';
-    require 'Ferrari.php';
-    require 'constant/CarConstants.php';
+require 'Car.php';
+require 'Honda.php';
+require 'Nissan.php';
+require 'Ferrari.php';
+require 'constant/CarConstants.php';
 
-    $honda = new Honda();
-    $nissan = new Nissan();
-    $ferrari = new Ferrari();
-    print_r($honda);
-    print_r($nissan);
-    print_r($ferrari);
+$honda = new Honda();
+$nissan = new Nissan();
+$ferrari = new Ferrari();
+
+function output(Car $car)
+{
+    echo $car->name . 'の価格は' . $car->get_price() . '円です。定員数は' .
+        $car->get_passenger() . '人です。加速力は' . $car->get_acceleration() . "s/mです。\n";
+}
+
+output($honda);
+output($nissan);
+output($ferrari);
