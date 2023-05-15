c5db80bcf37d8d995e471ebf9b5a038a010b9941
Cyber Neko <firexneko7@gmail.com>
2023-05-15 22:54:35 +0900

    加速力の単位の修正

--- output/question1.php
+++ output/question1.php
@@ -12,7 +12,7 @@ $ferrari = new Ferrari();
 function output(Car $car)
 {
     echo $car->name . 'の価格は' . $car->get_price() . '円です。定員数は' .
-        $car->get_passenger() . '人です。加速力は' . $car->get_acceleration() . "s/mです。\n";
+        $car->get_passenger() . '人です。加速力は' . $car->get_acceleration() . "m/sです。\n";
 }
 
 output($honda);
