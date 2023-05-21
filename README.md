fc3da283188ecc17557bb3afcff2baab61af2444
Cyber Neko <firexneko7@gmail.com>
2023-05-22 00:21:14 +0900

    intelephenseの機能でフォーマット

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # php_oop_test
+
 php_oop_test
 
 - constant
@@ -7,4 +8,3 @@ php_oop_test
   - クラス図、アクティビティ図を管理
 - output
   - 課題の出力結果を管理
-  
--- output/question2.php
+++ output/question2.php
@@ -1,12 +1,12 @@
 <?php
-    require 'Car.php';
-    require 'Ferrari.php';
-    require 'constant/CarConstants.php';
+require 'Car.php';
+require 'Ferrari.php';
+require 'constant/CarConstants.php';
 
-    $ferrari = new Ferrari();
-    echo 'リフトアップ前の加速力： ';
-    print_r($ferrari->get_acceleration());
-    echo PHP_EOL;
-    $ferrari->liftUp();
-    echo 'リフトアップ後の加速力： ';
-    print_r($ferrari->get_acceleration());
+$ferrari = new Ferrari();
+echo 'リフトアップ前の加速力： ';
+print_r($ferrari->get_acceleration());
+echo PHP_EOL;
+$ferrari->liftUp();
+echo 'リフトアップ後の加速力： ';
+print_r($ferrari->get_acceleration());
